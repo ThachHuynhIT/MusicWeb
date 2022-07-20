273ef72a05c2fb483550b0e6096dfeefb0fd1b31
ThachHuynhIT <75834703+ThachHuynhIT@users.noreply.github.com>
2022-07-20 20:24:48 +0700

    gitignore

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res)  {
+app.use(morgan('combined'))
 
-  return res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
