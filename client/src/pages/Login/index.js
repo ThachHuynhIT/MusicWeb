b0404d4e23b9fbd435e0d9d0e9be394faa4867a5
VuongHuy <96243959+VuongHuy2k1@users.noreply.github.com>
2022-09-02 13:38:35 +0700

    huy aditt

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,8 +1,5 @@
-<<<<<<< Updated upstream
 import { LoginLayout } from "../../components/Layouts/User";
-=======
-import { LoginLayout } from "../../components/Layouts/Account";
->>>>>>> Stashed changes
+
 import React from "react";
 import ReactDOM from "react-dom";
 function Login() {
