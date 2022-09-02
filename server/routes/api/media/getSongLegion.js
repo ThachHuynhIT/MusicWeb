468c56586db5b11843259cdbd5f32bef90a15324
ThachHuynhIT <75834703+ThachHuynhIT@users.noreply.github.com>
2022-09-02 14:04:41 +0700

    api mini update

--- server/routes/api/media/getSongLegion.js
+++ server/routes/api/media/getSongLegion.js
@@ -1,17 +1,20 @@
 const Song = require("../../../models/Song");
-const mongoose = require("mongoose");
 
 module.exports = (req, res, next) => {
   let perPage = 5;
   let page = req.params.page || 1;
 
-  Song.find({ legion: req.params.legion }) 
+  Song.find({ legion: req.params.legion })
     .skip(perPage * page - perPage)
     .limit(perPage)
     .exec((err, song) => {
       Song.countDocuments((err, count) => {
         if (err) return next(err);
-        res.send(song); 
+        res.send({
+          song,
+          current: page,
+          pages: Math.ceil(count / perPage), 
+        });
       });
     });
 };
