3f7fe50094af5be8698ec010741b35cc341ef204
ThachHuynhIT <75834703+ThachHuynhIT@users.noreply.github.com>
2022-09-04 11:04:27 +0700

    update api searchSongName

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,21 @@
 const admin = require("./admin");
 const api = require("./api");
-const verifyToken = require("../middlewares/verifyToken")
+const verifyToken = require("../middlewares/verifyToken");
 
 function route(app) {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
-    app.use("/api", api);
+  app.use("/api", api);
 
-    // app.use("/admin", verifyToken,admin);
-    app.use("/admin", admin);
-  
-  }
-  
-  module.exports = route;
+  // app.use("/admin", verifyToken,admin);
+  app.use("/admin", admin);
+}
+
+module.exports = route;
