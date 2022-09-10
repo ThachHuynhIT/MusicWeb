2cb61f533af412e2674a747ff60395efc91deefe
ThachHuynhIT <75834703+ThachHuynhIT@users.noreply.github.com>
2022-09-10 10:06:30 +0700

    api update img

--- server/routes/api/playlist/index.js
+++ server/routes/api/playlist/index.js
@@ -1,14 +1,11 @@
 const express = require("express");
+const upload = require("../../../middlewares/uploadMiddleware")
 
 const getPlayList = require("./getPlayList");
-
 const deletePlayList = require("./deletePlayList");
-
 const createPlayList = require("./createPlayList");
-
-const updatePlaylistImg = require("./createPlaylistImg");
 const getSongPlayList = require("./getSongPlayList");
-
+const updatePlaylistImg = require("./updatePlaylistImg");
 const addSongToPlayList = require("./addSongToPlayList");
 const deleteSongOfPlayList = require("./deleteSongOfPlayList");
 
@@ -21,7 +18,7 @@ router.get("/get-playlist/:userId", getPlayList);
 router.post("/create-playlist/:userId", createPlayList);
 router.delete("/delete-playlist/:playlistId", deletePlayList);
 router.get("/get-song-playlist/:playlistId", getSongPlayList);
-router.post("/update-playlist-img/:playlistId", updatePlaylistImg);
+router.put("/update-playlist-img/:playlistId",upload.single('image'), updatePlaylistImg);
 router.put("/add-song-playlist/:playlistId/:songId", addSongToPlayList);
 router.put("/delete-song-playlist/:playlistId/:songId", deleteSongOfPlayList);
 
--- server/routes/api/playlist/updatePlaylistImg.js
+++ server/routes/api/playlist/updatePlaylistImg.js
@@ -1,8 +1,21 @@
 const Playlist = require("../../../models/PlayList");
+const path = require("path");
+require("dotenv").config();
 
+const Resize = require("../../../middlewares/Resize");
 
-module.exports = (req, res, next) => {
-  Playlist.find({ _id: req.params.playlistId })
-    .then((playlist) => res.send(playlist))
-    .catch(next);
+module.exports = async function (req, res) {
+  const imagePath = path.join( "test/public/img");
+  // call class Resize
+  const fileUpload = new Resize(imagePath);
+  if (!req.file) {
+    res.status(401).json({ error: "Please provide an image" });
+  }
+  const filename = await fileUpload.save(req.file.buffer);
+  const img = path.join(process.env.LOCAL_PATH_STORE+filename)
+
+  Playlist.updateOne({_id: req.params.playlistId},{img:img})
+  .then()
+
+  return res.status(200).json({ url: img });
 };
